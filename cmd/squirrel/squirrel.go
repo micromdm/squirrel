f60ad559f674251cfd8a8f61d4407374fb30f06c
Victor Vrantchan <groob@users.noreply.github.com>
2017-10-21 01:46:27 -0400

    add tls config from go4/httputil (#10)

--- cmd/squirrel/squirrel.go
+++ cmd/squirrel/squirrel.go
@@ -1,7 +1,70 @@
 package main
 
-import "github.com/micromdm/squirrel/cli"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/micromdm/go4/version"
+)
+
+func runVersion(args []string) error {
+	version.PrintFull()
+	return nil
+}
+
+func usageFor(fs *flag.FlagSet, short string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, "  %s\n", short)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+		w := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(w, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		w.Flush()
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "USAGE\n")
+	fmt.Fprintf(os.Stderr, "  %s <mode> --help\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "MODES\n")
+	fmt.Fprintf(os.Stderr, "  serve        Run the server\n")
+	fmt.Fprintf(os.Stderr, "  version      Print full version information\n")
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "VERSION\n")
+	fmt.Fprintf(os.Stderr, "  %s\n", version.Version().Version)
+	fmt.Fprintf(os.Stderr, "\n")
+}
 
 func main() {
-	cli.Main()
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	var run func([]string) error
+	switch strings.ToLower(os.Args[1]) {
+	case "serve":
+		run = runServe
+	case "version":
+		run = runVersion
+	case "help", "-h", "--help":
+		usage()
+		return
+	default:
+		usage()
+		os.Exit(1)
+	}
+
+	if err := run(os.Args[2:]); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
